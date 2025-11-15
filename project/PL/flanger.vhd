d8cbc4bb45a1ea4714bf2c8cac66fdc3dcd12c07
unknown <archist250505@gmail.com>
2025-11-15 17:56:03 +1100

    dma stuck

---
+++ project/PL/flanger.vhd
@@ -0,0 +1,262 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/28/2025 11:18:27 AM
+-- Design Name: 
+-- Module Name: flanger - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity flanger is
+  generic (
+        DATA_WIDTH : positive := 32;
+        PCM_PRECISION : integer := 18
+    );
+  Port (
+    signal clk      : in std_logic;
+    signal rst      : in std_logic;
+    signal data_in  : in std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
+    signal data_out : out std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
+    signal data_in_ready : in std_logic;  --1 clk signal to indicate new input is ready
+    signal data_out_ready : out std_logic --1 clk signal to indicate new output is ready
+    --signal fifo_full      : in std_logic
+  );
+end flanger;
+architecture Behavioral of flanger is
+    --effect of flanger (g)
+    signal g_shift: integer := 2; --g = 0.5 (shift the sample by 2)
+    
+    --LFO wave, LFO delay is between 1ms and 10ms a.k.a. 48 to 480 samples
+    constant BASE_DELAY : integer := 48;
+    constant MIN_DELAY : integer := 48;
+    constant BUF_SIZE  : integer := 528;
+    constant MAX_DELAY : integer := 480;
+    constant LFO_HALF_PERIOD : integer := 9999936; --generate triangular LFO: 100 MHZ -> 5Hz: 1000000 * 100 / 5 / 2 --10000000
+    constant CLK_PER_STEP : integer := 23148;
+    signal clk_count      : integer range 0 to LFO_HALF_PERIOD := 0;
+    --signal clk_step_count : integer range 0 to CLK_PER_STEP - 1 := 0;
+    signal current_delay : integer range MIN_DELAY to MAX_DELAY := MIN_DELAY;
+    signal lfo_dir        : std_logic := '0'; --0 = up, 1 = down
+    signal all_clk_count  : integer := 0;
+    signal clk_count_per_step : integer range 0 to CLK_PER_STEP := 0;
+    
+    --let max delay be 10 ms, base delay = min delay be 1ms
+    type sample_array_t is array (0 to BUF_SIZE - 1) of std_logic_vector(DATA_WIDTH-1 downto 0); --480 samples = 10 ms, shift array
+    signal previous_samples : sample_array_t := (others => (others => '0'));
+    signal samples_ptr, delayed_index      : integer range 0 to BUF_SIZE - 1 := 0;
+    
+    --for arithmetics on the output of interpolation
+    signal delayed_sample_18 : signed(17 downto 0);
+    signal delayed_sample_scaled : signed(17 downto 0);
+    signal delayed_sample_ext : signed(31 downto 0);
+    signal zeros_14 : signed(13 downto 0) := (others => '0');
+    
+    --for interpolation
+    constant INTERPOL_MULTIPLY          : integer := 46385; --(2^30) / 23148 (16 bits) (46385 if round up)
+    constant INTERPOL_SHIFT_DIVIDE      : integer := 1073741824; --DIVIDE BY 2^30
+   
+    --pipeline registers
+    --stage 1
+    signal stage1_input                : std_logic_vector(31 downto 0);
+    signal new_sample_st1              : std_logic;
+    signal delayed_sample0_18bit, delayed_sample1_18bit       : signed(17 downto 0);
+    signal clk_count_per_step_st1      : integer range 0 to CLK_PER_STEP := 0;
+    
+    --stage 2
+    signal d_sample0_mult, d_sample1_mult   : signed(34 downto 0);
+    signal new_sample_st2              : std_logic;
+    signal stage2_input                : std_logic_vector(31 downto 0);
+    signal clk_count_per_step_st2      : integer range 0 to CLK_PER_STEP := 0;
+    
+    --stage 3
+    signal d_sample0_mult2, d_sample1_mult2 : signed(50 downto 0);
+    signal stage3_input                : std_logic_vector(31 downto 0);
+    signal new_sample_st3              : std_logic;
+    
+begin
+    --PIPELINE STAGE 1--
+    --PIPELINED REGISTERS STAGE 1
+    process(clk) 
+        variable d_index0, d_index1 : integer range 0 to BUF_SIZE - 1;
+    begin
+        if (rising_edge(clk)) then
+            --save current input
+            stage1_input <= data_in;
+            new_sample_st1 <= data_in_ready;
+            --indexes for samples
+            d_index0 := (samples_ptr - BASE_DELAY - current_delay + BUF_SIZE - 1) mod BUF_SIZE;
+            d_index1 := (samples_ptr - BASE_DELAY - current_delay + BUF_SIZE) mod BUF_SIZE;           
+            --extract previous samples that we will use in later stages
+            delayed_sample0_18bit <= signed(previous_samples(d_index0)(31 downto 14));
+            delayed_sample1_18bit <= signed(previous_samples(d_index1)(31 downto 14));
+            --clk count for cauclations
+            clk_count_per_step_st1 <= clk_count_per_step;
+        end if;
+    end process;
+    
+    --UPDATE BUFFER
+    process(clk) begin
+        --rising clock
+        if (rising_edge(clk)) then
+            --reset
+            if(rst = '1') then
+               previous_samples <= (others => (others => '0'));
+               samples_ptr <= 0;
+            --no reset
+            else
+                --if there is a new sample
+                if (data_in_ready = '1') then
+                    --if the buffer is full
+                    if (samples_ptr = BUF_SIZE - 1) then
+                        samples_ptr <= 0; --samples_ptr;
+                        previous_samples(samples_ptr) <= data_in;
+                    --if the buffer is not full
+                    else
+                        samples_ptr <= samples_ptr + 1;
+                        previous_samples(samples_ptr) <= data_in;
+                    end if;
+                --if there is no new sample
+                else
+                    samples_ptr <= samples_ptr;
+                end if;
+            end if;
+        end if;
+    end process;
+    
+    
+    --generate LFO
+    --TODO
+    process(clk, rst) begin
+        if (rst = '1') then
+            clk_count_per_step <= 0;
+            clk_count <= 0;
+        elsif(rising_edge(clk)) then
+            --clk count per step
+            if (clk_count_per_step < CLK_PER_STEP) THEN
+                clk_count_per_step <= clk_count_per_step + 1;
+            else
+                clk_count_per_step <= 0;
+            end if;
+        
+            if (lfo_dir = '0') then            
+                if (clk_count < LFO_HALF_PERIOD) then --was < LFO_HALF_PERIOD
+                clk_count <= clk_count + 1;
+                else
+                    clk_count <= clk_count;
+                    lfo_dir <= '1';
+                end if;
+            else
+                if (clk_count > 0) then
+                    clk_count <= clk_count - 1;
+                else
+                    clk_count <= clk_count;
+                    lfo_dir <= '0';
+                end if;
+            end if;
+        end if;
+    end process;
+    
+    --generate current delay based on LFO
+    --TODO
+    process(clk, rst) begin
+        if (rst = '1') then
+            current_delay <= 48;
+        elsif(rising_edge(clk)) then
+            if ((clk_count_per_step = CLK_PER_STEP)) then
+                if (lfo_dir = '0') then
+                    if (current_delay < 480) then
+                        current_delay <= current_delay + 1;
+                    else
+                        current_delay <= MAX_DELAY;
+                    end if;
+                else
+                    if (current_delay > 48) then
+                        current_delay <= current_delay - 1;
+                    else
+                        current_delay <= MIN_DELAY;
+                    end if;
+                end if;
+            else
+                current_delay <= current_delay;
+            end if;
+        end if;
+    end process;
+    
+    --calculate output
+    
+    --STAGE 2 (multiply by 2^30)
+    process(clk) begin
+        if (rising_edge(clk)) then
+            d_sample0_mult <= delayed_sample0_18bit * to_signed(INTERPOL_MULTIPLY, 17);
+            d_sample1_mult <= delayed_sample1_18bit * to_signed(INTERPOL_MULTIPLY, 17);
+            new_sample_st2 <= new_sample_st1;
+            stage2_input <= stage1_input;
+            clk_count_per_step_st2 <= clk_count_per_step_st1;
+        end if;
+    end process;
+    
+    --STAGE 3 (multiply by top of the fraction)
+    process(clk) begin
+        if (rising_edge(clk)) then
+            d_sample0_mult2 <= d_sample0_mult * (to_signed(CLK_PER_STEP, 16) - to_signed(clk_count_per_step_st2, 16)); --16 * 35
+            d_sample1_mult2 <= d_sample1_mult * to_signed(clk_count_per_step_st2, 16);
+            new_sample_st3 <= new_sample_st2;
+            stage3_input <= stage2_input;
+        end if;
+    end process;
+    
+    --STAGE 4
+    process(clk) 
+        variable d_sample_scaled : signed(17 downto 0);
+        variable d_sample_ext    : signed(31 downto 0);
+        variable d_sample0_div, d_sample1_div, d_samples_sum : signed(17 downto 0);
+    begin
+        if(rising_edge(clk)) then
+            --simply divide by 2^30, very fast shift
+            d_sample0_div := resize(shift_right(d_sample0_mult2, 30), 18); --resize(d_sample0_mult2 / to_signed(INTERPOL_SHIFT_DIVIDE, 31), 18); --was 31 bit
+            d_sample1_div := resize(shift_right(d_sample1_mult2, 30), 18); --resize(d_sample1_mult2 / to_signed(INTERPOL_SHIFT_DIVIDE, 31), 18);
+            --add
+            d_samples_sum := d_sample0_div + d_sample1_div;
+            --divide by 2
+            d_sample_scaled := d_samples_sum / 2;
+            d_sample_ext := d_sample_scaled & zeros_14;
+            
+            --return output
+            if ((new_sample_st3 = '1')) then
+                data_out_ready <= '1';
+            else
+                data_out_ready <= '0';
+            end if;
+            
+            if (new_sample_st3 = '1') then
+                 data_out <= std_logic_vector(signed(stage3_input) + d_sample_ext);
+            end if;
+        end if;
+    end process;
+        
+end Behavioral;
---
+++ project/PS/helloworld.c
@@ -0,0 +1,303 @@
+#include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+#include "xaxidma.h"
+#include "sleep.h"
+#include "ff.h"
+#include "xsdps.h"
+#include "xuartps.h"
+#include <string.h>
+#include "xparameters.h"
+#include "xil_io.h"
+#include "sleep.h"
+
+//for the button
+#define AXI_GPIO_SW_OFFSET  0x00000004
+//#define XPAR_AXI_GPIO_0_BASEADDR A0030000
+
+//for interacting with the audio PL
+#define AXI_AUDIO_SEL_OFFSET 0x00000000
+
+//STATE FOR FSM
+#define NONE 0	//the starting state
+#define ECHO 1	//echo audio effect
+#define GAIN 2	//gain audio effect
+#define SPEED 3	//speed audio effect
+#define CLIPPING 4 //clipping audio effect
+#define FLANGER 5  //flanger audio effect
+#define WRITE_FILE 6 //like NONE but at the beginning the file is written
+
+//file buffer
+#define BUF_SIZE 28800000 //48000 * 600 (time limit is 10 minutes)
+#define BATCH_SIZE 1024	  //we read 1024 samples at once
+#define SAMPLES_READ_AT_ONCE 5 //we read 5 * 1024 before checking the button every time
+volatile uint32_t rx_buf[BUF_SIZE] = {}; //declare a 10 minute buffer
+
+//For axi dma
+XAxiDma AxiDma; //DMA oject
+
+//for sd
+static FIL fil; //file object
+static FATFS fatfs; //filesystem object
+MKFS_PARM mkfs_parm; //parameters to make filesystem
+
+
+//constants
+#define SAMPLE_SIZE 32 //size of sample
+#define SAMPLE_PMC 18  //number of valid bits per sample
+#define EXTRA_BITS SAMPLE_SIZE - SAMPLE_PMC //the useless bits
+#define SAMPLE_RATE 48000 //sample rate of the i2s microphone
+
+//wav file header
+//source: http://soundfile.sapp.org/doc/WaveFormat/
+typedef struct {
+    char chunkID[4];        // "RIFF"
+    uint32_t chunkSize;     // 36 + data size
+    char format[4];         // "WAVE"
+    char subChunk1ID[4];    // "fmt "
+    uint32_t subchunk1Size; // 16
+    uint16_t audioFormat;   // 1 = PCM
+    uint16_t numChannels;   // 1 or 2
+    uint32_t sampleRate;    // e.g., 48000
+    uint32_t byteRate;      // sampleRate * numChannels * bitsPerSample/8
+    uint16_t blockAlign;    // numChannels * bitsPerSample/8
+    uint16_t bitsPerSample; // bits per sample (precision)
+    char subchunk2ID [4];   // "data"
+    uint32_t subchunk2Size; // bytes of PCM data
+} WAVHeader;
+
+
+//write into sd card function
+int sd_write(volatile uint32_t *buf, int buf_size, char *name) {
+ FRESULT res; //result of file operations
+ //open the file
+ res = f_open(&fil, name, FA_CREATE_ALWAYS | FA_WRITE);
+ unsigned int br;
+ if(res != FR_OK)
+   {
+  xil_printf("file creation failed, FRESULT=%d\n", res);
+  print("file creation failed\n");
+     return -1;
+   }
+
+ res = f_lseek(&fil, 0);
+ if (res) {
+  f_close(&fil);
+  return -1;
+ }
+
+ //HEADER
+ WAVHeader header;
+ memcpy(header.chunkID, "RIFF", 4);
+ memcpy(header.format, "WAVE", 4);
+ memcpy(header.subChunk1ID, "fmt ", 4);
+ memcpy(header.subchunk2ID, "data", 4);
+
+ header.subchunk1Size = 16;
+ header.audioFormat = 1;
+ header.numChannels = 1; //1 or 2
+ header.sampleRate = SAMPLE_RATE; //sample rate
+ header.bitsPerSample = 32;
+ header.blockAlign = header.numChannels
+      * header.bitsPerSample / 8;
+ header.byteRate = header.sampleRate
+       * header.blockAlign;
+ header.subchunk2Size = buf_size * 4;
+ header.chunkSize = 36 + header.subchunk2Size;
+
+ // Write WAV header
+ res = f_write(&fil, &header, sizeof(WAVHeader), &br);
+ if (res != FR_OK || br != sizeof(WAVHeader)) {
+     print("failed to write header\n");
+     f_close(&fil);
+     return - 1;
+  }
+
+
+ //write data
+ res = f_write(&fil, (const void*)buf, buf_size * 4, &br) ;
+ if(res != FR_OK){
+  print("file write failed\n");
+  	 f_close(&fil);
+     return -1 ;
+ }
+
+ res = f_close(&fil);
+ if (res != FR_OK) {
+  return -1;
+ }
+
+ print("file successfully written\n");
+}
+
+//configure the dma
+int dma_config() {
+    //declare necessary variables
+	int Status = XST_SUCCESS;
+	XAxiDma_Config *CfgPtr;
+
+	//no scatter-gather
+	CfgPtr = XAxiDma_LookupConfig(XPAR_AXI_DMA_0_DEVICE_ID);
+	if (!CfgPtr) {
+		print("No CfgPtr");
+	    return XST_FAILURE;
+	}
+
+	//init configuration
+	Status = XAxiDma_CfgInitialize(&AxiDma, CfgPtr);
+	    if (Status != XST_SUCCESS) {
+	        print("DMA cfg init failure");
+	        return XST_FAILURE;
+	   }
+
+	   if (XAxiDma_HasSg(&AxiDma)) {
+	       print("Device configured as SG mode \r\n");
+	       return XST_FAILURE;
+	   }
+
+	   print("DMA initialised\r\n");
+
+	   //self test to check if it is configured properly
+	   Status = XAxiDma_Selftest(&AxiDma);
+	   if (Status != XST_SUCCESS) {
+	         print("DMA failed selftest\r\n");
+	         return XST_FAILURE;
+	    }
+	    print("DMA passed self test\r\n");
+
+	    //polling rather than interrupts
+	    XAxiDma_IntrDisable(&AxiDma, XAXIDMA_IRQ_ALL_MASK,
+	    XAXIDMA_DEVICE_TO_DMA);
+	    XAxiDma_IntrDisable(&AxiDma, XAXIDMA_IRQ_ALL_MASK,
+	    XAXIDMA_DMA_TO_DEVICE);
+
+        return XST_SUCCESS;
+}
+
+//read from the dma
+void dma_read() {
+	int Status = XST_SUCCESS;
+	//READ SAMPLES_READ_AT_ONCE * BUF_SIZE samples
+	for (int i = 0; i < SAMPLES_READ_AT_ONCE; i++) {
+	    Xil_DCacheFlushRange((UINTPTR)(rx_buf + i * BATCH_SIZE), BATCH_SIZE * 4);
+	    //sleep(5); //maybe remove
+	    //read from DMA
+	    Status = XAxiDma_SimpleTransfer(&AxiDma, (UINTPTR) (rx_buf + i * BATCH_SIZE),
+	            BATCH_SIZE * 4, XAXIDMA_DEVICE_TO_DMA);
+
+	    if (Status != XST_SUCCESS) {
+	    	print("failed rx transfer call\r\n");
+	    }
+	    else {
+	    	//print("dma read success\r\n");
+	    }
+
+	    while (XAxiDma_Busy(&AxiDma, XAXIDMA_DEVICE_TO_DMA)) {
+	    	}
+	    	//print("DMA read completed\r\n");
+
+	    	//flush
+	    	//invalidate instead of flush
+	    	Xil_DCacheFlushRange((UINTPTR)(rx_buf + i * BATCH_SIZE), BATCH_SIZE * 4);
+	    }
+
+	    //print("DMA done\r\n");
+
+}
+
+int new_state(uint32_t current_state) {
+    switch (current_state) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            return current_state + 1;
+        case 6:
+            return 1;
+        default:
+            return 0; // optional: handle unexpected values
+    }
+}
+
+//configure the sd card
+int config_sd() {
+    //mount sd
+    FRESULT rc;
+    rc = f_mount(&fatfs, "0:/", 1);
+    if (rc != FR_OK) {
+    	print("failed to mount sd card\n");
+        return rc;
+    }
+    else {
+    	print("sd is mounted\n");
+    }
+
+    //IMPORTANT: make file system: only needs to be done ONCE. Don't do this on every boot.
+    //uncomment if you use the sd card for the first time
+    //it can take a lot of time (minutes)
+
+    //mkfs_parm.fmt = FM_FAT32;
+    //BYTE work[FF_MAX_SS];
+    //rc = f_mkfs("0:/", &mkfs_parm , work, sizeof work);
+
+    if (rc != FR_OK) {
+        return rc;
+    	xil_printf("f_mkfs failed, FRESULT=%d\n", rc);
+    }
+    else {
+    	xil_printf("successfully made the file system\n");
+    }
+
+    return rc;
+}
+
+int main() {
+
+ uint32_t state = NONE;
+ int buf_stored_size = 0;
+ int file_written_count = 0;
+ char filename[3] = "aud"; //4 chars for file index, 1 for '\0'
+ char full_filename[8];
+
+//button state
+int button_state = 0;
+int button_state_prev = 0;
+
+ dma_config();
+ config_sd();
+
+ while(1) {
+	 //get the new file name
+    snprintf(full_filename, sizeof(full_filename), "%s%d", filename, file_written_count);
+    //read the samples from dma
+    xil_printf("going to dma\n");
+    dma_read();
+    xil_printf("leaving the dma\n");
+    //updated the buffer size
+    buf_stored_size += BUF_SIZE * SAMPLES_READ_AT_ONCE;
+    //if the buffer is filled
+    if (buf_stored_size == BUF_SIZE) {
+    	//write the file, update file counter and reset the buffer size
+        sd_write(rx_buf, buf_stored_size, full_filename);
+        buf_stored_size = 0;
+        file_written_count += 1;
+    }
+    //check the button
+    button_state_prev = button_state;
+    button_state = Xil_In32(XPAR_AXI_GPIO_0_BASEADDR //A0030000
+		    	  	  	  	+ AXI_GPIO_SW_OFFSET);
+
+    if ((button_state != button_state_prev) && (button_state == 0)) {
+        state = new_state(state);
+        xil_printf("new state = %d\n", state);
+        Xil_Out32(XPAR_AUDIO_PIPELINE_0_BASEADDR + AXI_AUDIO_SEL_OFFSET, state);
+        if (state == WRITE_FILE) {
+        	sd_write(rx_buf, buf_stored_size, full_filename);
+        	buf_stored_size = 0;
+        	file_written_count += 1;
+        }
+    }
+ }
+}
